6d8c932fcb61d87d2cb9b37a4cdd18cc1e9959d5
Danial Eizlan Daud <64958824+deezlan@users.noreply.github.com>
2025-11-21 14:53:41 +0000

    Implement Task 3a function

--- SimulateSensorData_Timer/__init__.py
+++ SimulateSensorData_Timer/__init__.py
@@ -1,13 +1,26 @@
+import datetime
 import logging
+import os
+import requests
+import azure.functions as func
 
-from azure.functions import TimerRequest
-from datetime import datetime, timezone
+def main(mytimer: func.TimerRequest) -> None:
+    start_time = datetime.datetime.now(datetime.timezone.utc)
+    logging.info(f"[T1] Task 3a Timer trigger started at {start_time.isoformat()}")
 
+    # Make internal call to Task 1's HTTP function
+    try:
+        function_url = os.environ.get("FUNC_URL_GENERATE_SENSOR_DATA")
+        if not function_url:
+            raise ValueError("FUNC_URL_GENERATE_SENSOR_DATA not set in environment.")
 
-def main(mytimer: TimerRequest) -> None:
-    utc_timestamp = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
+        response = requests.get(function_url)
+        
+        end_time = datetime.datetime.now(datetime.timezone.utc)
+        delta = (end_time - start_time).total_seconds()
+        
+        logging.info(f"Task 1 HTTP function response: {response.status_code} - {response.text}")
+        logging.info(f"[T2] Task 1 HTTP function responded at {end_time.isoformat()} (Delta: {delta:.3f} seconds)") # Log time taken for graph plotting
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    except Exception as e:
+        logging.error(f"Failed to trigger GenerateSensorData HTTP function: {e}")
